a0bd3a2d6216c932afa0c06e9847bb394d7e4a77
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2022-01-12 08:31:28 +0530

    first commit

--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,10 +1,54 @@
-import {useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
-import Container from '../../components/common/Container';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import {TouchableOpacity} from 'react-native';
+import Icon from '../../components/common/Icon';
+import ContactsComponent from '../../components/ContactsComponent';
+import getContacts from '../../context/actions/contacts/getContacts';
+import {GlobalContext} from '../../context/Provider';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Contacts = () => {
+  const [sortBy, setSortBy] = React.useState(null);
   const {setOptions, toggleDrawer} = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+  const contactsRef = useRef([]);
+  const {
+    contactsDispatch,
+    contactsState: {
+      getContacts: {data, loading},
+    },
+  } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getContacts()(contactsDispatch);
+  }, []);
+
+  const getSettings = async () => {
+    const sortPref = await AsyncStorage.getItem('sortBy');
+    if (sortPref) {
+      setSortBy(sortPref);
+    }
+  };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      getSettings();
+      return () => {};
+    }, []),
+  );
+
+  useEffect(() => {
+    const prev = contactsRef.current;
+    contactsRef.current = data;
+    const newList = contactsRef.current;
+    if (newList.length - prev.length === 1) {
+      const newContact = newList.find(
+        item => !prev.map(i => i.id).includes(item.id),
+      );
+      navigate(CONTACT_DETAIL, {item: newContact});
+    }
+  }, [data.length]);
+
   React.useEffect(() => {
     setOptions({
       headerLeft: () => (
@@ -12,15 +56,20 @@ const Contacts = () => {
           onPress={() => {
             toggleDrawer();
           }}>
-          <Text style={{padding: 10}}>NAV</Text>
+          <Icon type="material" style={{padding: 10}} size={25} name="menu" />
         </TouchableOpacity>
       ),
     });
   }, []);
+
   return (
-    <Container>
-      <Text>Hi from Contacts</Text>
-    </Container>
+    <ContactsComponent
+      modalVisible={modalVisible}
+      setModalVisible={setModalVisible}
+      data={data}
+      loading={loading}
+      sortBy={sortBy}
+    />
   );
 };
 
